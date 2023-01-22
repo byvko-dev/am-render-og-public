a187f5a735e66efe31d57d6761e1caa0f622e7ff
Cufee <vkouzin@outlook.com>
2023-01-22 16:11:49 -0500

    added api

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # am-render-og
 
 This repository includes two services that are required in order to render stats images for Aftermath.
-- `webapp` is a NextJS app straight from the OG tutorial with JSX components required to render the UI and one API route that returns a base64 encoded png image.
 
-- `service` is a small api service written in Go which is used to validate the input and do any required pre/post processing. Everything that is done by this service can be done as part of the NextJS app, but I am not a fan of JS and I prefer Go in case I integrate this with gRPC later on. 
+#
+### `webapp` 
+Is a NextJS app straight from the OG tutorial with JSX components required to render the UI and one API route that returns a base64 encoded png image.
+- This app makes a request to an existing Aftermath API that actually generates player session and all the `Block` elements. This service is a closed source for now, but feel free to make your own logic to actually create the UI inside `webapp/src/core/components/render/StatsContainer.tsx`.
+#  
+### `service`
+Is a small api service written in Go which is used to validate the input and do any required pre/post processing. Everything that is done by this service can be done as part of the NextJS app, but I am not a fan of JS and I prefer Go in case I integrate this with gRPC later on. 
 
+Here is a sample request:  
+`POST: http://localhost:9098/v1/session/player?debug=true`  
+```
+{
+    "account_id": 1034813981,
+    "realm": "na",
+    "locale": "eng",
+    "preset": "legacy",
+    "days":0
+}
+```
+_Remove `?debug=true` to get a JSON response_
--- service/main.go
+++ service/main.go
@@ -2,38 +2,79 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"image/png"
 	"io/ioutil"
 
+	"github.com/byvko-dev/am-core/helpers/env"
 	"github.com/byvko-dev/am-render-og/internal/render"
-
-	stats "github.com/byvko-dev/am-types/api/stats/v1"
+	api "github.com/byvko-dev/am-types/api/generic/v1"
+	"github.com/byvko-dev/am-types/api/stats/v1"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	// Setup a server
-	// app := fiber.New()
+	app := fiber.New()
 
-	// app.Use(logger.New())
+	app.Use(logger.New())
 
-	// v1 := app.Group("/v1")
+	v1 := app.Group("/v1")
 
-	// session := v1.Group("/session")
-	// // session.Post("/player", handlers.GetPlayerSession)
+	session := v1.Group("/session")
+	session.Post("/player", playerSessionImageHandler)
 
-	// port := env.MustGetString("PORT")
-	// panic(app.Listen(":" + port))
+	port := env.MustGetString("PORT")
+	panic(app.Listen(":" + port))
+}
 
-	img, err := render.GetStatsImage(stats.RequestPayload{AccountID: 1013072123, Realm: "NA"})
+func playerSessionImageHandler(c *fiber.Ctx) error {
+	var response api.ResponseWithError
+	var request stats.RequestPayload
+
+	err := c.BodyParser(&request)
 	if err != nil {
-		panic(err)
+		response.Error.Message = err.Error()
+		response.Error.Context = "failed to parse request"
+		response.Error.Code = fiber.StatusBadRequest
+		c.Status(fiber.StatusBadRequest).JSON(response)
 	}
 
-	var buf bytes.Buffer
-	err = png.Encode(&buf, img)
+	img, err := render.GetStatsImage(request)
 	if err != nil {
-		panic(err)
+		response.Error.Message = err.Error()
+		response.Error.Context = "failed to render image"
+		response.Error.Code = fiber.StatusInternalServerError
+		c.Status(fiber.StatusInternalServerError).JSON(response)
 	}
 
-	err = ioutil.WriteFile("test.png", buf.Bytes(), 0644)
+	// Encode image as png
+	buf := new(bytes.Buffer)
+	err = png.Encode(buf, img)
+	if err != nil {
+		response.Error.Message = err.Error()
+		response.Error.Context = "failed to encode image"
+		response.Error.Code = fiber.StatusInternalServerError
+		c.Status(fiber.StatusInternalServerError).JSON(response)
+	}
+
+	if c.Query("debug") == "true" {
+		// This is for debugging purposes, so we can see the image in the browser/postman
+		c.Set("Content-Type", "image/png")
+		return c.Send(buf.Bytes())
+	}
+
+	// Encode image as base64
+	s, err := ioutil.ReadAll(buf)
+	if err != nil {
+		response.Error.Message = err.Error()
+		response.Error.Context = "failed to read image"
+		response.Error.Code = fiber.StatusInternalServerError
+		c.Status(fiber.StatusInternalServerError).JSON(response)
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(s)
+	response.Data = encoded
+	return c.JSON(response)
 }
